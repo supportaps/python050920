04065613106fdf8e29f2d7f4d3aacc5e0d8c683c
Jaroslav Kovalenko <Rvanuyked@gmail>
2020-09-18 18:04:44 +0300

    added third execise

--- ua/univer/base/day2/lesson02/hw02/cycle_for_while.py
+++ ua/univer/base/day2/lesson02/hw02/cycle_for_while.py
@@ -7,11 +7,27 @@ def the_bug_collector_problem():
         errors = int(input("How many errors are today? ")) + errors
     print(" The total number of errors is: ", errors)
 
+def the_birned_callories():
+
+    callories_per_min = 4.2
+    step = 5
+    for i in range(10, 30 + step, step):
+        print("Birned callories per", i, "minutes is: ", i * callories_per_min)
 
 
+def budget_analyze():
 
-
-
+    sum_per_month = int(input("Input total sum for one month: "))
+    number_of_expenses = int(input("Input number of expences for per one month: "))
+    step = 1
+    sum_expences = 0
+    for i in range(1, number_of_expenses + step, step):
+        name_of_the_expance, value_expanse = input("Input name and value: ").split()
+        sum_expences = sum_expences + int(value_expanse)
+    if sum_expences > sum_per_month:
+        print(" The expanse is overspared ", sum_expences  - sum_per_month)
+    else:
+        print(" The rest you saved: ", sum_per_month - sum_expences)
 
 if __name__== '__main__':
-    the_bug_collector_problem()
+    budget_analyze()
