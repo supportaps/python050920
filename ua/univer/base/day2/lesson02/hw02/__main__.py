c3b895d1f2a828855ed28d63f3e8440fe1cdaeb9
Jaroslav Kovalenko <Rvanuyked@gmail>
2020-09-19 10:42:08 +0300

    added second ex to func

--- ua/univer/base/day2/lesson02/hw02/__main__.py
+++ ua/univer/base/day2/lesson02/hw02/__main__.py
@@ -3,10 +3,17 @@ from ua.univer.base.day2.lesson02.hw02 import functions
 
 def main():
 
-    km = int(input("Input kilometres: "))
-    result = functions.convert_km_to_miles(km)
-    print("Returned value in miles: ", result)
+    answer = int(input("Input number: "))
 
+    if answer == 1:
+        km = int(input("Input kilometres: "))
+        result = functions.convert_km_to_miles(km)
+        print("Returned value in miles: ", result)
+    elif answer == 2:
+        total_purchase = float(input(" Input a purchase: "))
+        FEDERAL_TAX = 0.025
+        REGIONAL_TAX = 0.05
+        result = functions.tax_from_selling(total_purchase, FEDERAL_TAX, REGIONAL_TAX)
 
 if __name__ == "__main__":
     main()
